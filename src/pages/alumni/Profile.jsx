74d1c5fed9369fc8626100284e8c98ee370ef800
Iswarya P <iswarya83p@gmail.com>
2026-04-16 09:41:54 +0530

    Import UseRef in Profile component

--- src/pages/alumni/Profile.jsx
+++ src/pages/alumni/Profile.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState , UseRef } from "react";
 import { X } from "lucide-react";
 import { getMyProfile, updateProfile } from "../../services/apiService";
 import Button from "../../components/Button";
